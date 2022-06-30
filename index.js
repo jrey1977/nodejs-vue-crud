af034f1cbc21c801660fbd0a3bd9387abc8731ff
Javier Rey <jrey@festina.com>
2022-06-30 16:46:39 +0200

    Cambios en los directorios

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const hbs = create({
     extname: ".hbs",
 });
 
+
 app.engine(".hbs", hbs.engine);
 app.set("view engine", ".hbs");
 app.set("views", "./views");
@@ -14,6 +15,11 @@ app.set("views", "./views");
 app.get("/", (req, res) => {
     res.render("home");
 });
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
 app.use(express.static(__dirname + "/public"));
 
 app.listen(3000, () => {
